9d997d339e025550c718ae6741129ef0d38f1d9b
LEANDRO LACHINI NASCIMENTO <leandrolachini@gmail.com>
2024-10-03 13:27:04 +0000

    Criando dimensão data utilizando os pacotes dbt_utils e dbt_date. Para aprendizado criei a dimensão data de 2 formas.

--- models/sales_department/marts/dim_date.sql
+++ models/sales_department/marts/dim_date.sql
@@ -1,8 +1,22 @@
-with
-    int_date_union as (
-        select *
-        from {{ ref('int_date_union') }}
-    )
+with 
+     raw_generated_data as ( 
+        {{ dbt_date.get_date_dimension("2015-01-01", "2050-12-31") }} 
+    ) 
 
-select *
-from int_date_union
+    , portuguese_renamed as ( 
+        select 
+             raw_generated_data. *
+             , case 
+                when raw_generated_data.day_of_week =  1  then 'Domingo' 
+                when raw_generated_data.day_of_week =  2  then 'Segunda-feira' 
+                when raw_generated_data.day_of_week =  3  then 'Terça-feira' 
+                when raw_generated_data.day_of_week =  4  then 'Quarta-feira' 
+                when raw_generated_data.day_of_week =  5  then 'Quinta-feira' 
+                when raw_generated_data.day_of_week =  6  then 'Sexta-feira' 
+                when raw_generated_data.day_of_week =  7  then 'Sábado' 
+            end as day_of_week_name_pt 
+        from raw_generated_data 
+    ) 
+
+select  * 
+from portuguese_renamed
